574323eff2fa42ccaed7d0145eb752262e338b77
Almir Rogério Camolesi <camolesi@fema.edu.br>
2025-03-25 16:57:42 -0300

    Atualiza Final Insert
    Atualiza Final Insert

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
 import React, { useState, useEffect}  from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logoCadastro from './assets/cadastro.png';
 
 function App() {
-  const baseUrl="https://localhost:7024/api/Alunos";
+
+  const baseUrl="https://localhost:7024/api/alunos";
   const [data, setData]=useState([]);
 
+  const [modalEditar, setModalEditar]=useState(false);
   const [modalIncluir, setModalIncluir]=useState(false);
+  const [modalExcluir, setModalExcluir]=useState(false);
 
   const [alunoSelecionado, setAlunoSelecionado]=useState({
     id: '',
@@ -26,12 +30,13 @@ function App() {
     console.log(alunoSelecionado);
   }
 
-//-----modal controle do estado 
-const abrirFecharModalIncluir=()=>{
-  setModalIncluir(!modalIncluir);
-}
+  //-----modal controle do estado 
+  const abrirFecharModalIncluir=()=>{
+    setModalIncluir(!modalIncluir);
+  }
 
-  const alunosGet= async()=>{
+
+  const alunoGet=async()=>{
     await axios.get(baseUrl)
     .then(response=>{
       setData(response.data);
@@ -40,7 +45,7 @@ const abrirFecharModalIncluir=()=>{
     })
   }
 
-  const pedidoPost=async()=>{
+  const alunoPost=async()=>{
     delete alunoSelecionado.id;
     alunoSelecionado.idade=parseInt(alunoSelecionado.idade);
       await axios.post(baseUrl, alunoSelecionado)
@@ -52,53 +57,50 @@ const abrirFecharModalIncluir=()=>{
     })
   }
 
-
-
   useEffect(()=>{
-    alunosGet();
+    alunoGet();
   })
 
   return (
     <div className="aluno-container">
-      <br/>
-      <h3>Cadastro de Alunos</h3>
-
+       <br/>
+       <h3>Cadastro de Alunos</h3>
       <header>
-     
+        <img src={logoCadastro} alt="Cadastro" class="img-fluid"  />
         <button onClick={()=>abrirFecharModalIncluir()} className="btn btn-success">Incluir Novo Aluno</button>
        </header>
-
-      <table className="table table-bordered">
-          <thead>
-             <tr>
-                 <th>ID</th>
-                 <th>Nome</th>
-                 <th>E-Mail</th>
-                 <th>Idade</th>
-             </tr>
-          </thead>
-
-          <tbody>
+      <table className="table table-bordered" >
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
           {data.map(aluno=>(
             <tr key={aluno.id}>
               <td>{aluno.id}</td>
               <td>{aluno.nome}</td>
               <td>{aluno.email}</td>
               <td>{aluno.idade}</td>
-
+              <td>
+                  BTN
+              </td>
               </tr>
           ))}
         </tbody>
       </table>
-
-
+      
       <Modal isOpen={modalIncluir}>
       <ModalHeader>Incluir Alunos</ModalHeader>
       <ModalBody>
         <div className="form-group">
           <label>Nome: </label>
           <br />
-          <input type="text" className="form-control" nname="nome"  onChange={handleChange}/>
+          <input type="text" className="form-control" name="nome"  onChange={handleChange}/>
           <br />
           <label>Email: </label>
           <br />
@@ -111,7 +113,7 @@ const abrirFecharModalIncluir=()=>{
         </div>
       </ModalBody>
       <ModalFooter>
-        <button className="btn btn-primary" onClick={()=>pedidoPost()}>Incluir</button>{"   "}
+        <button className="btn btn-primary" onClick={()=>alunoPost()}>Incluir</button>{"   "}
         <button className="btn btn-danger" onClick={()=>abrirFecharModalIncluir()}>Cancelar</button>
       </ModalFooter>
     </Modal>
