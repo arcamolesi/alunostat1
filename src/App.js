856fc72884a14bebe156d7551d4de764647013c9
Almir Rogério Camolesi <camolesi@fema.edu.br>
2025-03-25 21:00:27 -0300

    Código Final de CRUD Aluno - Completo
    Código Final de CRUD Aluno - Completo

--- src/App.js
+++ src/App.js
@@ -35,8 +35,15 @@ function App() {
     setModalIncluir(!modalIncluir);
   }
 
+  const abrirFecharModalEditar=()=>{
+    setModalEditar(!modalEditar);
+  }
 
-  const alunoGet=async()=>{
+   const abrirFecharModalExcluir=()=>{
+    setModalExcluir(!modalExcluir);
+  }
+
+  const pedidoGet=async()=>{
     await axios.get(baseUrl)
     .then(response=>{
       setData(response.data);
@@ -45,7 +52,7 @@ function App() {
     })
   }
 
-  const alunoPost=async()=>{
+  const pedidoPost=async()=>{
     delete alunoSelecionado.id;
     alunoSelecionado.idade=parseInt(alunoSelecionado.idade);
       await axios.post(baseUrl, alunoSelecionado)
@@ -57,8 +64,44 @@ function App() {
     })
   }
 
+  const alunoPut=async()=>{
+    alunoSelecionado.idade=parseInt(alunoSelecionado.idade);
+    await axios.put(baseUrl+"/"+alunoSelecionado.id, alunoSelecionado)
+    .then(response=>{
+      var resposta=response.data;
+      var dadosAuxiliar=data;
+      //eslint-disable-next-line
+      dadosAuxiliar.map(aluno=>{
+        if(aluno.id===alunoSelecionado.id){
+          aluno.nome=resposta.nome;
+          aluno.email=resposta.email;
+          aluno.idade=resposta.idade;
+        }
+      });
+      abrirFecharModalEditar();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  const alunoDelete=async()=>{
+    await axios.delete(baseUrl+"/"+alunoSelecionado.id)
+    .then(response=>{
+     setData(data.filter(aluno=>aluno.id!==response.data));
+      abrirFecharModalExcluir();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  const selecionarAluno=(aluno, caso)=>{
+    setAlunoSelecionado(aluno);
+      (caso==="Editar")?
+        abrirFecharModalEditar(): abrirFecharModalExcluir();
+  }
+
   useEffect(()=>{
-    alunoGet();
+    pedidoGet();
   })
 
   return (
@@ -66,7 +109,7 @@ function App() {
        <br/>
        <h3>Cadastro de Alunos</h3>
       <header>
-        <img src={logoCadastro} alt="Cadastro" class="img-fluid"  />
+        <img src={logoCadastro} alt="Cadastro" />
         <button onClick={()=>abrirFecharModalIncluir()} className="btn btn-success">Incluir Novo Aluno</button>
        </header>
       <table className="table table-bordered" >
@@ -87,7 +130,8 @@ function App() {
               <td>{aluno.email}</td>
               <td>{aluno.idade}</td>
               <td>
-                  BTN
+              <button className="btn btn-primary" onClick={()=>selecionarAluno(aluno, "Editar")}>Editar</button> {"  "}
+              <button className="btn btn-danger" onClick={()=>selecionarAluno(aluno, "Excluir")}>Excluir</button>
               </td>
               </tr>
           ))}
@@ -113,11 +157,54 @@ function App() {
         </div>
       </ModalBody>
       <ModalFooter>
-        <button className="btn btn-primary" onClick={()=>alunoPost()}>Incluir</button>{"   "}
+        <button className="btn btn-primary" onClick={()=>pedidoPost()}>Incluir</button>{"   "}
         <button className="btn btn-danger" onClick={()=>abrirFecharModalIncluir()}>Cancelar</button>
       </ModalFooter>
     </Modal>
 
+    <Modal isOpen={modalEditar}>
+      <ModalHeader>Editar Aluno</ModalHeader>
+      <ModalBody>
+        <div className="form-group">
+        <label>ID: </label>
+          <br />
+          <input type="text" className="form-control" readOnly value={alunoSelecionado && alunoSelecionado.id}/>
+          <br />
+          <label>Nome: </label>
+          <br />
+          <input type="text" className="form-control" name="nome" onChange={handleChange} value={alunoSelecionado && alunoSelecionado.nome}/>
+          <br />
+          <label>Email: </label>
+          <br />
+          <input type="text" className="form-control" name="email" onChange={handleChange} value={alunoSelecionado && alunoSelecionado.email}/>
+          <br />
+          <label>Idade: </label>
+          <br />
+          <input type="text" className="form-control" name="idade" onChange={handleChange} value={alunoSelecionado && alunoSelecionado.idade}/>
+          <br />
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <button className="btn btn-primary" onClick={()=>alunoPut()}>Editar</button>{"   "}
+        <button className="btn btn-danger" onClick={()=>abrirFecharModalEditar()}>Cancelar</button>
+      </ModalFooter>
+    </Modal>
+
+    <Modal isOpen={modalExcluir}>
+        <ModalBody>
+        Confirma a exclusão deste(a) aluno(a) : {alunoSelecionado && alunoSelecionado.nome} ?
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-danger" onClick={()=>alunoDelete()}>
+            Sim
+          </button>
+          <button
+            className="btn btn-secondary" onClick={()=>abrirFecharModalExcluir()}
+          >
+            Não
+          </button>
+        </ModalFooter>
+      </Modal>
 
     </div>
   );
